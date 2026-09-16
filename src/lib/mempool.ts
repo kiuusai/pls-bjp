87d41b38a2671efb70e70c26cf00f7bffb56b3fb
kiuusai <kiuusai@protonmail.com>
2026-09-16 15:59:00 -0300

    Using @mempool/mempool.js lib into mempool utils folder instead of fetching it manually

--- src/lib/mempool.ts
+++ src/lib/mempool.ts
@@ -1,115 +1,76 @@
-export interface UTXO {
-	txid: string;
-	vout: number;
-	value: number;
-}
+import mempoolJs from '@mempool/mempool.js';
+import type { MempoolConfig } from '@mempool/mempool.js/lib/interfaces';
+import type { AddressTxsUtxo } from '@mempool/mempool.js/lib/interfaces/bitcoin/addresses';
+
+export type UTXO = AddressTxsUtxo;
 
 // const MEMPOOL_API_URL = getMempoolAPIUrl();
 
-export function createMempoolApi(network: { isLiquid: boolean; isTestnet: boolean }) {
-	const start = network.isLiquid ? 'https://blockstream.info' : 'https://mempool.space';
+type Network = {
+	isLiquid: boolean;
+	isTestnet: boolean;
+	custom?: MempoolConfig;
+};
 
-	const end = network.isTestnet
-		? network.isLiquid
-			? `/liquidtestnet/api`
-			: `/testnet/api`
-		: network.isLiquid
-			? `/liquid/api`
-			: `/api`;
+export function createMempoolApi(network: Network) {
+	function createMempoolConfig(network: Network): MempoolConfig {
+		if (network.custom) return network.custom;
 
-	const url = start + end;
+		const protocol = 'https';
+
+		if (network.isLiquid) {
+			const hostname = 'liquid.network'
+
+			if (network.isTestnet) {
+				return {
+					protocol,
+					hostname,
+					network: 'liquidtestnet',
+				};
+			}
+
+			return {
+				protocol,
+				hostname,
+				network: 'liquid',
+			};
+		}
+
+		const hostname = 'mempool.space';
+
+		if (network.isTestnet) {
+			return {
+				protocol,
+				hostname,
+				network: 'testnet'
+			}
+		}
+
+		return {
+			protocol,
+			hostname,
+			network: 'mainnet',
+		}
+	}
+
+	const mempool = mempoolJs(createMempoolConfig(network));
+
+	const api = network.isLiquid ? mempool.liquid : mempool.bitcoin;
 
 	return {
-		async getTransactionHexFromId(txId: string) {
-			try {
-				const res = await fetch(`${url}/tx/${txId}/hex`);
-
-				if (!res.ok) {
-					const text = await res.text();
-					console.log(text);
-					return null;
-				}
-
-				return await res.text();
-			} catch (error) {
-				alert(
-					`Couldn't connect to mempool.space. Try disabling your adblocker or try using another browser`
-				);
-				console.log(error);
-				return null;
-			}
+		async getTransactionHexFromId(txid: string) {
+			return await api.transactions.getTxHex({ txid });
 		},
 
 		async getAddressUtxos(address: string) {
-			try {
-				const res = await fetch(`${url}/address/${address}/utxo`);
-
-				if (!res.ok) {
-					const text = await res.text();
-					console.log(text);
-					return null;
-				}
-
-				return (await res.json()) as UTXO[];
-			} catch (error) {
-				alert(
-					`Couldn't connect to mempool.space. Try disabling your adblocker or try using another browser`
-				);
-				console.log(error);
-				return null;
-			}
+			return await api.addresses.getAddressTxsUtxo({ address });
 		},
 
 		async getAddressUnconfirmedTxs(address: string) {
-			try {
-				const res = await fetch(`${url}/address/${address}/txs/mempool`);
-
-				if (!res.ok) {
-					const text = await res.text();
-					console.log(text);
-					return null;
-				}
-
-				return (await res.json()) as {
-					txid: string;
-					vin: {
-						txid: string;
-						vout: number;
-						prevout: {
-							scriptpubkey_address: string;
-							value: number;
-						};
-						sequence: number;
-					}[];
-				}[];
-			} catch (error) {
-				alert(
-					`Couldn't connect to mempool.space. Try disabling your adblocker or try using another browser`
-				);
-				console.log(error);
-				return null;
-			}
+			return await api.addresses.getAddressTxsMempool({ address });
 		},
-		async publishTransaction(transactionHex: string) {
-			try {
-				const res = await fetch(`${url}/tx`, {
-					method: 'POST',
-					body: transactionHex
-				});
-
-				const text = await res.text();
-
-				if (res.ok) {
-					alert(`Transaction published! TxId: ${text}`);
-					return text;
-				} else {
-					alert(`Error: ${text}`);
-					return null;
-				}
-			} catch (error) {
-				alert('Error while publishing! Try disabling your adblocker or try using another browser');
-				return null;
-			}
+		async publishTransaction(txhex: string) {
+			return await api.transactions.postTx({ txhex });
 		}
 	};
 }
